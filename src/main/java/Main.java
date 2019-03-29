089663c27d6a566eefa68e7bc8a6f0fee5541f26
Анна <annadoshenko@mail.ru>
2019-03-29 18:22:12 +0300

    Corrected assertion, made next task completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 
 public class Main {
@@ -16,12 +19,71 @@ public class Main {
     WebElement element = ((ChromeDriver) driver).findElementByClassName("wg-btn--green");
     click(element, driver);
     RandomGenerator randomGenerator = new RandomGenerator();
-    String email = randomGenerator.generateRandom(10)+"+wpt@wriketask.qaa";
+    String email = randomGenerator.generateRandomString(10)+"+wpt@wriketask.qaa";
     element = ((ChromeDriver) driver).findElementByClassName("modal-form-trial__input");
     element.sendKeys(email);
     element = ((ChromeDriver) driver).findElementByClassName("modal-form-trial__submit");
     click(element, driver);
-    assert driver.getCurrentUrl() != urlStart;
-
+    try {
+      Thread.sleep(2500);
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    Assert.assertFalse((driver.getCurrentUrl().equals(urlStart)));
+    int variant = randomGenerator.generateRandomInt(1, 2);
+    switch (variant)
+    {
+      case(1):
+        element = ((ChromeDriver) driver).findElementByCssSelector("label[for=interest_in_solution_1]");
+        break;
+      case (2):
+        element = ((ChromeDriver) driver).findElementByCssSelector("label[for=interest_in_solution_2]");
+        break;
+    }
+    click(element, driver);
+    variant = randomGenerator.generateRandomInt(1, 5);
+    switch (variant)
+    {
+      case(1):
+        element = driver.findElement(By.xpath("//button[text()='1-5']"));
+        break;
+      case (2):
+        element = driver.findElement(By.xpath("//button[text()='6-15']"));
+        break;
+      case (3):
+        element = driver.findElement(By.xpath("//button[text()='16-25']"));
+        break;
+      case (4):
+        element = driver.findElement(By.xpath("//button[text()='26-50']"));
+        break;
+      case (5):
+        element = driver.findElement(By.xpath("//button[text()='50+']"));
+        break;
+    }
+    click(element, driver);
+    element = ((ChromeDriver) driver).findElementByClassName("js-button");
+    click(element, driver);
+    variant = randomGenerator.generateRandomInt(1, 2);
+    switch (variant)
+    {
+      case(1):
+        element = driver.findElement(By.xpath("//button[text()='Yes']"));
+        break;
+      case (2):
+        element = driver.findElement(By.xpath("//button[text()='No']"));
+        break;
+      case (3):
+        element = driver.findElement(By.xpath("//button[text()='Other']"));
+        break;
+    }
+    click(element, driver);
+    try {
+      Thread.sleep(1000);
+    }
+    catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    Assert.assertFalse(((ChromeDriver) driver).findElementByClassName("again").isDisplayed());
   }
 }
--- src/main/java/RandomGenerator.java
+++ src/main/java/RandomGenerator.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class RandomGenerator {
   private static final String characters = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
 
-  public static String generateRandom(int length) {
+  public static String generateRandomString(int length) {
     Random random = new SecureRandom();
     if (length <= 0) {
       throw new IllegalArgumentException("String length must be a positive integer");
@@ -16,5 +16,14 @@ public class RandomGenerator {
     }
 
     return sb.toString();
+  }
+  public static int generateRandomInt(int minimum, int maximum)
+  {
+    Random rn = new Random();
+    int n = maximum - minimum + 1;
+    int i = rn.nextInt() % n;
+    int randomNum =  minimum + i;
+    return randomNum;
+
   }
 }
